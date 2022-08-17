c1aacb707a4d9692a3522cd2b241b0dabecd203d
jaydenthomas <jayden@ionrev.com>
2022-08-17 16:29:47 -0600

    Change Vue to app; conditionally load components

--- src/install.js
+++ src/install.js
@@ -3,19 +3,26 @@ import { Ability, subject } from '@casl/ability';
 
 import * as components from './components';
 
-const install = (Vue, {prefix} = {}) => {
+const install = (app, {prefix, loadComponents = true} = {}) => {
   for (let key in components) {
     let _key = prefix ? prefix + key : key;
-    Vue.component(_key, components[key]);
+    app.component(_key, components[key]);
   }
 
-  Vue.use(abilitiesPlugin, new Ability);
+  if (loadComponents) {
+    for (let key in components) {
+      let _key = prefix ? prefix + key : key;
+      app.component(_key, components[key]);
+    }
+  }
+
+  app.use(abilitiesPlugin, new Ability);
   // Vue.prototype.$createEntity = function (path, item) {
   //   const classFn =  new Function('item', `return new class ${path} {constructor(args) {Object.assign(this, item)}}`);
   //   const newClass = classFn(item);
   //   return newClass;
   // };
-  Vue.prototype.$subject = subject;
+  app.prototype.$subject = subject;
 };
 
 // auto install
